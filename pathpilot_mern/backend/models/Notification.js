c5afb29df8583a3a20eecb37881a223fec16fc2f
Ram Prakash Kurmi <Prataapsingh98@gmail.com>
2026-04-18 23:15:13 +0530

    Add Notification model with schema definition

--- pathpilot_mern/backend/models/Notification.js
+++ pathpilot_mern/backend/models/Notification.js
@@ -1,1 +1,12 @@
-  
+  import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    message: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Notification', notificationSchema);
