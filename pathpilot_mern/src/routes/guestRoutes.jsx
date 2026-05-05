5f8392414a93c72a7d65923ba2d211d2aea6f325
Maheshwar Chaudhary <156220825+maheshwarchaudhary@users.noreply.github.com>
2026-05-05 20:45:55 +0530

    Update guestRoutes.jsx

--- pathpilot_mern/src/routes/guestRoutes.jsx
+++ pathpilot_mern/src/routes/guestRoutes.jsx
@@ -1,1 +1,26 @@
+import { Route } from "react-router-dom"
+import PublicRoute from "../components/guards/PublicRoute"
+
+
+import GuestHome from '../pages/guest/GuestHome'
+import GuestAbout from '../pages/guest/GuestAbout'
+import GuestFeatures from '../pages/guest/GuestFeatures'
+import GuestCareerPath from '../pages/guest/GuestCareerPath'
+import GuestResources from '../pages/guest/GuestResources'
+import GuestContact from '../pages/guest/GuestContact'
+import GuestCourseDetail from '../pages/guest/GuestCourseDetail'
+import PathFinder from '../pages/user/PathFinder'
+
+export const guestRoutes = (
+  <Route element={<PublicRoute />}>
+    <Route path="/guest/home" element={<GuestHome />} />
+    <Route path="/guest/about" element={<GuestAbout />} />
+    <Route path="/guest/features" element={<GuestFeatures />} />
+    <Route path="/guest/career-paths" element={<GuestCareerPath />} />
+    <Route path="/guest/resources" element={<GuestResources />} />
+    <Route path="/guest/contact" element={<GuestContact />} />
+    <Route path="/guest/course/:title" element={<GuestCourseDetail />} />
+    <Route path="/path-finder" element={<PathFinder />} />
+  </Route>
+)
   
